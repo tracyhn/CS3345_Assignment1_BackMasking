515c97137b443b6558493b5871e2f15be81800fc
tracyhuynh <thh220000@utdallas.edu>
2023-02-15 21:04:33 -0600

    second push

--- ListStackDriver.java
+++ ListStackDriver.java
@@ -15,6 +15,6 @@ public class ListStackDriver {
         System.out.println("Now there should be zero items in the stack: " + stack.count());
         System.out.println("Should throw an exception:");
         stack.peek();
-        System.out.println();
+        System.out.println("Some test here");
     }
 }
