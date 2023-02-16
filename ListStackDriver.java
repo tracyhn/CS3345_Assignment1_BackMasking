7086e8c7a02a2af9a0052da8e9f8b098c976caab
tracyhuynh <thh220000@utdallas.edu>
2023-02-15 21:03:34 -0600

    first commit

--- ListStackDriver.java
+++ ListStackDriver.java
@@ -15,5 +15,6 @@ public class ListStackDriver {
         System.out.println("Now there should be zero items in the stack: " + stack.count());
         System.out.println("Should throw an exception:");
         stack.peek();
+        System.out.println();
     }
 }
